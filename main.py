a6b05c1d8df2351be5da1f7ec43b842723359b55
Navodith <navodithmanoj171106@gmail.com>
2022-12-15 17:05:20 +0300

    Main file uploaded

---
+++ main.py
@@ -0,0 +1,34 @@
+#import kivy
+import random
+movelist = ["Rock", "Paper", "Scissors"]
+playerSelectedMove = ""
+
+#Makes Random AI move and calcualtes the winner
+def calculateWinner():
+    selectedMove = movelist[random.randint(0,2)]
+    print("Opponent chooses", selectedMove)
+    selectedMove = selectedMove.lower()
+    if playerSelectedMove == selectedMove:
+        print(f"Both players selected {playerSelectedMove}. It's a tie!")
+    elif playerSelectedMove == "rock":
+        if selectedMove == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif playerSelectedMove == "paper":
+        if selectedMove == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif playerSelectedMove == "scissors":
+        if selectedMove == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+        
+print("Welcome to rock paeper scissors game maen")
+while True:    
+    playerSelectedMove = input("Choose a Move: Rock, Paeper, Scissors: ")
+    playerSelectedMove = playerSelectedMove.lower()
+    calculateWinner()
