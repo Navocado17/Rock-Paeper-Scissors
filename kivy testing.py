3262883d20ae527a51c7a91ea12b6fb459814228
Don Roy Chacko <donisepic30@gmail.com>
2022-12-15 23:52:52 +0300

    The basic scenes created

--- kivy testing.py
+++ kivy testing.py
@@ -2,19 +2,38 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class MainMenu(BoxLayout):
+class MainMenu(Screen):
+    def quitGame(self):
+        import sys
+        sys.exit()
+
+
+class Game(Screen):
     pass
+
+class Settings(Screen):
+    pass
+
+
 
 class MainWidget(Widget):
     pass
 
 
 class RockPaperScissorsGame2022(App):
-    pass
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MainMenu(name='menu'))
+        sm.add_widget(Game(name='game'))
+        sm.add_widget(Settings(name='settings'))
+        return sm
 
 sound = SoundLoader.load('SOUND/angrybirdstheme.mp3')
 if sound:
     sound.play()
+    
 RockPaperScissorsGame2022().run()
