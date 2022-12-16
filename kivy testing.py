1f52f74782594e22c582b284d15a80696d499c2b
Navodith <navodithmanoj171106@gmail.com>
2022-12-16 13:14:47 +0300

    volume slider (work in progress)  and transitions

--- kivy testing.py
+++ kivy testing.py
@@ -3,6 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import CardTransition
 
 
 class MainMenu(Screen):
@@ -10,23 +11,18 @@ class MainMenu(Screen):
         import sys
         sys.exit()
 
-
 class Game(Screen):
     pass
 
 class Settings(Screen):
-    pass
-
-
-
-class MainWidget(Widget):
-    pass
+    def on_slider_value(self,widget):
+        print("Volume: " + str(widget.value))
 
 
 class RockPaperScissorsGame2022(App):
     def build(self):
         # Create the screen manager
-        sm = ScreenManager()
+        sm = ScreenManager(transition=CardTransition())
         sm.add_widget(MainMenu(name='menu'))
         sm.add_widget(Game(name='game'))
         sm.add_widget(Settings(name='settings'))
