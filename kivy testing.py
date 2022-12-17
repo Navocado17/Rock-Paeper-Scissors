5279708e86499d254749a9cd4afa40f58c7acec6
Don Roy Chacko <donisepic30@gmail.com>
2022-12-17 14:03:42 +0300

    Game Screen Update
    Images and buttons added buttons now can chift thru images. Padding is a mess rn though

--- kivy testing.py
+++ kivy testing.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import SlideTransition
 
@@ -12,7 +13,13 @@ class MainMenu(Screen):
         sys.exit()
 
 class Game(Screen):
-    pass
+    playerSideImage = StringProperty("assets/rock.jpg")
+    def rock(self):
+        self.playerSideImage = "assets/rock.jpg" 
+    def paper(self):
+        self.playerSideImage = "assets/paper.jpg" 
+    def scissors(self):
+        self.playerSideImage = "assets/scissors.jpg" 
 
 class Settings(Screen):
     def on_slider_value(self,widget):
@@ -33,5 +40,5 @@ sound = SoundLoader.load('SOUND/angrybirdstheme.mp3')
 if sound:
     sound.loop = True
     sound.play()
-
+    
 RockPaperScissorsGame2022().run()
