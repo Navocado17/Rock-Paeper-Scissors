1224cd8138ae7b60ca7cdcb0243165459f8d2ce0
Navodith <navodithmanoj171106@gmail.com>
2022-12-17 10:55:01 +0300

    changed transition back to slide

--- kivy testing.py
+++ kivy testing.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.screenmanager import CardTransition
+from kivy.uix.screenmanager import SlideTransition
 
 
 class MainMenu(Screen):
@@ -23,8 +23,7 @@ class Settings(Screen):
 
 class RockPaperScissorsGame2022(App):
     def build(self):
-        # Create the screen manager
-        sm = ScreenManager(transition=CardTransition())
+        sm = ScreenManager(transition=SlideTransition())
         sm.add_widget(MainMenu(name='menu'))
         sm.add_widget(Game(name='game'))
         sm.add_widget(Settings(name='settings'))
