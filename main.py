328f02f1ec3c94ad3aca3d7b4d506a02f6e572d5
Don Roy Chacko <donisepic30@gmail.com>
2023-01-09 20:27:46 +0300

    Loading Screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
+from kivy.uix.video import Video
 import time
 import random
 from kivy.properties import StringProperty
@@ -184,18 +185,18 @@ class MainMenu(Screen):
         else:
             self.background = "assets/reactions/eddie/9.png"
         return super().on_pre_enter(*args)
-    
 class LoadingScreen(Screen):
+   
     def wait(self, *args):
         self.manager.current = "menu"
     def on_pre_enter(self, *args):
-        Clock.schedule_once(self.wait, 3)
+        Clock.schedule_once(self.wait, 15)
         return super().on_pre_enter(*args)
-    def on_leave(self, *args):
+    def on_pre_leave(self, *args):
         if sound:
             sound.loop = True
             sound.play()
-        return super().on_leave(*args)
+        return super().on_pre_leave(*args)
 class RockPaeperScissorsGame2022(App):
     def build(self):
         self.icon = "assets/icon.png"
