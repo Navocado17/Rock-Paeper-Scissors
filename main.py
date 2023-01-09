1c49236240ce339678ef0767feda9978b064a50c
Don Roy Chacko <donisepic30@gmail.com>
2023-01-09 19:48:45 +0300

    Eddie Mode Complete

--- main.py
+++ main.py
@@ -16,21 +16,31 @@ Config.set('graphics', 'window_state', 'maximized')
 global sound
 sound = SoundLoader.load('SOUND/angrybirdstheme.mp3')
 
-class MainMenu(Screen):
-    def quitGame(self):
-        import sys
-        sys.exit()
+
 
 class Game(Screen):
     playerSideImage = StringProperty("assets/rock.jpg")
     opponentImage = StringProperty("assets/loading.jpg")
     movesList  = ["rock", "paper", "scissors"]
     opponentCharacterImage = StringProperty("assets/opponent.jpg")
+    opponentThinking = StringProperty("assets/thinking.jpg")
+    eddieVoicelines = ["You cant beat me maen", "the only thing they fear is me maen", "ill onepump u maen", "back to class maen", "sit ur place maen", "quit the game maen"]
     playerChoice = "rock"
     opponentChoice = ""
     confirmed = BooleanProperty(False)
+    eddieMode = False
     message = ""
     gameResult = ""
+    def on_pre_enter(self, *args):
+        settings = self.manager.get_screen('settings')
+        self.eddieMode = settings.eddieMode
+        print(self.eddieMode)
+        if self.eddieMode:
+            self.opponentCharacterImage = "assets/reactions/eddie/6.jpg"
+            self.opponentThinking = "assets/reactions/eddie/3.jpg"
+        return super().on_pre_enter(*args)
+        
+        
     def rock(self):
         self.playerSideImage = "assets/rock.jpg"
         self.playerChoice = "rock"
@@ -41,22 +51,34 @@ class Game(Screen):
         self.playerSideImage = "assets/scissors.jpg"
         self.playerChoice = "scissors"
     def confirm(self):
-        self.opponentCharacterImage = "assets/thinking.jpg"
+        self.opponentCharacterImage = self.opponentThinking
         self.confirmed = True
         self.opponentImage = "assets/loading.jpg"
         
         def delayedWork(*l):
-            self.opponentChoice = self.movesList[random.randint(0, 2)]
-            if self.opponentChoice == "rock":
-                self.opponentImage = "assets/rock_FLIPPED.jpg"
-            elif self.opponentChoice == "paper":
-                self.opponentImage = "assets/paper_FLIPPED.jpg"
-            elif self.opponentChoice == "scissors":
-                self.opponentImage = "assets/scissors_FLIPPED.jpg"  
-            self.opponentCharacterImage = "assets/opponent.jpg"
+            if self.eddieMode == True:
+                if self.playerChoice == "rock":
+                    self.opponentImage = "assets/paper_FLIPPED.jpg"
+                elif self.playerChoice == "paper":
+                    self.opponentImage = "assets/scissors_FLIPPED.jpg"
+                elif self.playerChoice== "scissors":
+                    self.opponentImage = "assets/rock_FLIPPED.jpg" 
+                self.opponentCharacterImage = "assets/reactions/eddie/6.jpg"
+            else:
+                self.opponentChoice = self.movesList[random.randint(0, 2)]
+                if self.opponentChoice == "rock":
+                    self.opponentImage = "assets/rock_FLIPPED.jpg"
+                elif self.opponentChoice == "paper":
+                    self.opponentImage = "assets/paper_FLIPPED.jpg"
+                elif self.opponentChoice == "scissors":
+                    self.opponentImage = "assets/scissors_FLIPPED.jpg" 
+                self.opponentCharacterImage = "assets/opponent.jpg"
             def show_result(*l):
                 message = ""
-                if self.playerChoice == self.opponentChoice:
+                if self.eddieMode == True:
+                    message = self.eddieVoicelines[random.randint(0, len(self.eddieVoicelines)-1)]
+                    self.gameResult = "e"
+                elif self.playerChoice == self.opponentChoice:
                     message = f"Both players selected {self.playerChoice}. It's a tie!"
                     self.gameResult = "t"
                 elif self.playerChoice == "rock":
@@ -84,7 +106,10 @@ class Game(Screen):
                 self.confirmed = False
                 self.manager.transition = RiseInTransition()
                 self.manager.current = "outcome"
-                
+                if self.eddieMode:
+                    self.opponentCharacterImage = "assets/reactions/eddie/6.jpg"
+                else:
+                    self.opponentCharacterImage = "assets/opponent.jpg"
                 
             Clock.schedule_once(show_result, 2)
         Clock.schedule_once(delayedWork, 2)
@@ -95,6 +120,7 @@ class Result(Screen):
     loseImages = ["assets/reactions/lose/1.jpg", "assets/reactions/lose/2.jpg", "assets/reactions/lose/3.jpg", "assets/reactions/lose/4.jpg"]
     winImages = ["assets/reactions/win/1.jpg", "assets/reactions/win/2.jpg", "assets/reactions/win/3.jpg", "assets/reactions/win/4.jpg"]
     tieImages = ["assets/reactions/tie/1.jpg", "assets/reactions/tie/2.jpg", "assets/reactions/tie/3.jpg", "assets/reactions/tie/4.jpg"]
+    eddieImages = ["assets/reactions/eddie/1.png", "assets/reactions/eddie/2.png", "assets/reactions/eddie/4.jpg", "assets/reactions/eddie/5.jpg", "assets/reactions/eddie/7.jpg", "assets/reactions/eddie/8.jpg"]
     def on_pre_enter(self, *args):
         gameWindow = self.manager.get_screen('game')
         self.result_text = gameWindow.message
@@ -104,6 +130,8 @@ class Result(Screen):
             self.image = self.loseImages[random.randint(0, (len(self.loseImages) - 1))]
         elif gameWindow.gameResult == "t":
             self.image = self.tieImages[random.randint(0, (len(self.tieImages) - 1))]
+        elif gameWindow.gameResult == "e":
+            self.image = self.eddieImages[random.randint(0, (len(self.eddieImages) - 1))]
         return super().on_pre_enter(*args)
 
     def returnToGame(self):
@@ -112,6 +140,7 @@ class Result(Screen):
 class Settings(Screen):
     eddieMode = False
     eddieSound = SoundLoader.load("SOUND/badpiggiestheme.mp3")
+    background = StringProperty("assets/settings.png")
     def on_slider_value(self,widget):
         if sound:
             sound.volume = widget.value
@@ -121,28 +150,62 @@ class Settings(Screen):
         print("Switch:",str(widget.active))
         if(widget.active):
             self.eddieMode = True
+            def jumpscare(*l):
+                self.background = 'assets/reactions/eddie/2.png'
+                
+            Clock.schedule_once(jumpscare, 1)
+            print(self.eddieMode)
             if sound:
                 sound.stop()
+
     
             if self.eddieSound:
                 self.eddieSound.loop=True
                 self.eddieSound.play()
         else:
+            self.eddieMode = False
+            self.background = 'assets/settings.png'
             if self.eddieSound:
                 self.eddieSound.stop()
             if sound:
                 sound.play()
+
+class MainMenu(Screen):
+    eddieMode = False
+    background = StringProperty("")
+    def quitGame(self):
+        import sys
+        sys.exit()
+    def on_pre_enter(self, *args):
+        settings = self.manager.get_screen('settings')
+        self.eddieMode = settings.eddieMode
+        if self.eddieMode == False:
+            self.background = "assets/background1.png"
+        else:
+            self.background = "assets/reactions/eddie/9.png"
+        return super().on_pre_enter(*args)
+    
+class LoadingScreen(Screen):
+    def wait(self, *args):
+        self.manager.current = "menu"
+    def on_pre_enter(self, *args):
+        Clock.schedule_once(self.wait, 3)
+        return super().on_pre_enter(*args)
+    def on_leave(self, *args):
+        if sound:
+            sound.loop = True
+            sound.play()
+        return super().on_leave(*args)
 class RockPaeperScissorsGame2022(App):
     def build(self):
         self.icon = "assets/icon.png"
         sm = ScreenManager(transition=SlideTransition())
+        sm.add_widget(LoadingScreen(name = 'loading'))
         sm.add_widget(MainMenu(name='menu'))
-        sm.add_widget(Game(name='game'))
         sm.add_widget(Settings(name='settings'))
+        sm.add_widget(Game(name='game'))
         sm.add_widget(Result(name='outcome'))
         return sm
-if sound:
-    sound.loop = True
-    sound.play()
+
     
 RockPaeperScissorsGame2022().run()
